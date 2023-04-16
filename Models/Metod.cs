08947b9837b9701846181de1a09043ef97bfee48
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-04-16 23:00:24 +0700

    Add Method.cs and Property.cs

---
+++ Models/Metod.cs
@@ -0,0 +1,34 @@
+﻿using Avalonia.Controls;
+using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DiagramClass.Models
+{
+    public class Metod : AbstractNotifyPropertyChanged
+    {
+        private string access;
+        private string myReturn;
+        private string name;
+        public string Аccess
+        {
+            get => access;
+            set => SetAndRaise(ref access, value);
+        }
+        public string Return
+        {
+            get => myReturn;
+            set => SetAndRaise(ref myReturn, value);
+        }
+
+        public string Name
+        {
+            get => name;
+            set => SetAndRaise(ref name, value);
+        }
+    }
+}
---
+++ Models/Properti.cs
@@ -0,0 +1,32 @@
+﻿using DynamicData.Binding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramClass.Models
+{
+    public class Properti : AbstractNotifyPropertyChanged
+    {
+        private string access;
+        private string myReturn;
+        private string name;
+        public string Аccess
+        {
+            get => access;
+            set => SetAndRaise(ref access, value);
+        }
+        public string Return
+        {
+            get => myReturn;
+            set => SetAndRaise(ref myReturn, value);
+        }
+
+        public string Name
+        {
+            get => name;
+            set => SetAndRaise(ref name, value);
+        }
+    }
+}
