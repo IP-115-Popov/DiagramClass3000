648a426deee6999e0df6c9d7247261f715d8afb9
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-04-27 13:03:14 +0700

    Add Control from one connector

--- Views/ConnectorsView/InheritanceConnector.axaml.cs
+++ Views/ConnectorsView/InheritanceConnector.axaml.cs
@@ -2,19 +2,19 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 
-namespace DiagramClass.Views
+namespace DiagramClass.Views.ConnectorsView
 {
-    public class AddictionConnectorView : TemplatedControl
+    public class InheritanceConnector : TemplatedControl
     {
         public static readonly StyledProperty<Point?> StartPointCastomProperty =
-          AvaloniaProperty.Register<AddictionConnectorView, Point?>("StartPointCastom");
+         AvaloniaProperty.Register<InheritanceConnector, Point?>("StartPointCastom");
         public Point? StartPointCastom
         {
             get => GetValue(StartPointCastomProperty);
             set => SetValue(StartPointCastomProperty, value);
         }
         public static readonly StyledProperty<Point?> EndPointCastomProperty =
-        AvaloniaProperty.Register<AddictionConnectorView, Point?>("EndPointCastom");
+        AvaloniaProperty.Register<InheritanceConnector, Point?>("EndPointCastom");
         public Point? EndPointCastom
         {
             get => GetValue(EndPointCastomProperty);
